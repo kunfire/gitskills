f8df58c355df2220686929d38c79c7fa85aacd2f
kunfire <kunfire@126.com>
2019-07-30 11:06:36 +0800

    test push

--- spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
+++ spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 	
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World , Jenkins......";
+        return "Hello World , Jenkins ...... test push";
     }
 }
